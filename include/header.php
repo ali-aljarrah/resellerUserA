5c039569ea06331e0cfa5678300f05ed3ef2e498
zainab98alrez <zainab.98ta@gmail.com>
2024-06-12 17:16:31 +0300

    hover icons

--- include/header.php
+++ include/header.php
@@ -4,10 +4,12 @@
 		<!--begin::Logo-->
 		<div class="app-header-logo d-flex flex-center">
 			<!--begin::Logo image-->
-			<a href="index.html">
-				<img alt="Logo" src="assets/media/logos/demo-58.svg" class="mh-25px" />
+			<div class="p-3 rounded-1 bg-blue">
+			<a href="http://resellerusera.test/">
+				<img alt="Logo" src="assets/img/LOGO.svg" class="mh-25px" />
 			</a>
 			<!--end::Logo image-->
+			</div>
 			<!--begin::Sidebar toggle-->
 			<button class="btn btn-icon btn-sm btn-active-color-primary d-flex d-lg-none" id="kt_app_sidebar_mobile_toggle">
 				<i class="ki-outline ki-abstract-14 fs-1"></i>
--- include/sidebar.php
+++ include/sidebar.php
@@ -12,246 +12,7 @@
                     </span>
                 </span>
                 <!--end:Menu link-->
-                <!--begin:Menu sub-->
-                <div class="menu-sub menu-sub-dropdown px-2 py-4 w-250px mh-75 overflow-auto">
-                    <!--begin:Menu item-->
-                    <div class="menu-item">
-                        <!--begin:Menu content-->
-                        <div class="menu-content">
-                            <span class="menu-section fs-5 fw-bolder ps-1 py-1">Home</span>
-                        </div>
-                        <!--end:Menu content-->
-                    </div>
-                    <!--end:Menu item-->
-                    <!--begin:Menu item-->
-                    <div class="menu-item">
-                        <!--begin:Menu link-->
-                        <a class="menu-link active" href="index.html">
-                            <span class="menu-bullet">
-                                <span class="bullet bullet-dot"></span>
-                            </span>
-                            <span class="menu-title">Default</span>
-                        </a>
-                        <!--end:Menu link-->
-                    </div>
-                    <!--end:Menu item-->
-                    <!--begin:Menu item-->
-                    <div class="menu-item">
-                        <!--begin:Menu link-->
-                        <a class="menu-link" href="dashboards/ecommerce.html">
-                            <span class="menu-bullet">
-                                <span class="bullet bullet-dot"></span>
-                            </span>
-                            <span class="menu-title">eCommerce</span>
-                        </a>
-                        <!--end:Menu link-->
-                    </div>
-                    <!--end:Menu item-->
-                    <!--begin:Menu item-->
-                    <div class="menu-item">
-                        <!--begin:Menu link-->
-                        <a class="menu-link" href="dashboards/projects.html">
-                            <span class="menu-bullet">
-                                <span class="bullet bullet-dot"></span>
-                            </span>
-                            <span class="menu-title">Projects</span>
-                        </a>
-                        <!--end:Menu link-->
-                    </div>
-                    <!--end:Menu item-->
-                    <!--begin:Menu item-->
-                    <div class="menu-item">
-                        <!--begin:Menu link-->
-                        <a class="menu-link" href="dashboards/online-courses.html">
-                            <span class="menu-bullet">
-                                <span class="bullet bullet-dot"></span>
-                            </span>
-                            <span class="menu-title">Online Courses</span>
-                        </a>
-                        <!--end:Menu link-->
-                    </div>
-                    <!--end:Menu item-->
-                    <!--begin:Menu item-->
-                    <div class="menu-item">
-                        <!--begin:Menu link-->
-                        <a class="menu-link" href="dashboards/marketing.html">
-                            <span class="menu-bullet">
-                                <span class="bullet bullet-dot"></span>
-                            </span>
-                            <span class="menu-title">Marketing</span>
-                        </a>
-                        <!--end:Menu link-->
-                    </div>
-                    <!--end:Menu item-->
-                    <div class="menu-inner flex-column collapse" id="kt_app_sidebar_menu_dashboards_collapse">
-                        <!--begin:Menu item-->
-                        <div class="menu-item">
-                            <!--begin:Menu link-->
-                            <a class="menu-link" href="dashboards/bidding.html">
-                                <span class="menu-bullet">
-                                    <span class="bullet bullet-dot"></span>
-                                </span>
-                                <span class="menu-title">Bidding</span>
-                            </a>
-                            <!--end:Menu link-->
-                        </div>
-                        <!--end:Menu item-->
-                        <!--begin:Menu item-->
-                        <div class="menu-item">
-                            <!--begin:Menu link-->
-                            <a class="menu-link" href="dashboards/pos.html">
-                                <span class="menu-bullet">
-                                    <span class="bullet bullet-dot"></span>
-                                </span>
-                                <span class="menu-title">POS System</span>
-                            </a>
-                            <!--end:Menu link-->
-                        </div>
-                        <!--end:Menu item-->
-                        <!--begin:Menu item-->
-                        <div class="menu-item">
-                            <!--begin:Menu link-->
-                            <a class="menu-link" href="dashboards/call-center.html">
-                                <span class="menu-bullet">
-                                    <span class="bullet bullet-dot"></span>
-                                </span>
-                                <span class="menu-title">Call Center</span>
-                            </a>
-                            <!--end:Menu link-->
-                        </div>
-                        <!--end:Menu item-->
-                        <!--begin:Menu item-->
-                        <div class="menu-item">
-                            <!--begin:Menu link-->
-                            <a class="menu-link" href="dashboards/logistics.html">
-                                <span class="menu-bullet">
-                                    <span class="bullet bullet-dot"></span>
-                                </span>
-                                <span class="menu-title">Logistics</span>
-                            </a>
-                            <!--end:Menu link-->
-                        </div>
-                        <!--end:Menu item-->
-                        <!--begin:Menu item-->
-                        <div class="menu-item">
-                            <!--begin:Menu link-->
-                            <a class="menu-link" href="dashboards/website-analytics.html">
-                                <span class="menu-bullet">
-                                    <span class="bullet bullet-dot"></span>
-                                </span>
-                                <span class="menu-title">Website Analytics</span>
-                            </a>
-                            <!--end:Menu link-->
-                        </div>
-                        <!--end:Menu item-->
-                        <!--begin:Menu item-->
-                        <div class="menu-item">
-                            <!--begin:Menu link-->
-                            <a class="menu-link" href="dashboards/finance-performance.html">
-                                <span class="menu-bullet">
-                                    <span class="bullet bullet-dot"></span>
-                                </span>
-                                <span class="menu-title">Finance Performance</span>
-                            </a>
-                            <!--end:Menu link-->
-                        </div>
-                        <!--end:Menu item-->
-                        <!--begin:Menu item-->
-                        <div class="menu-item">
-                            <!--begin:Menu link-->
-                            <a class="menu-link" href="dashboards/store-analytics.html">
-                                <span class="menu-bullet">
-                                    <span class="bullet bullet-dot"></span>
-                                </span>
-                                <span class="menu-title">Store Analytics</span>
-                            </a>
-                            <!--end:Menu link-->
-                        </div>
-                        <!--end:Menu item-->
-                        <!--begin:Menu item-->
-                        <div class="menu-item">
-                            <!--begin:Menu link-->
-                            <a class="menu-link" href="dashboards/social.html">
-                                <span class="menu-bullet">
-                                    <span class="bullet bullet-dot"></span>
-                                </span>
-                                <span class="menu-title">Social</span>
-                            </a>
-                            <!--end:Menu link-->
-                        </div>
-                        <!--end:Menu item-->
-                        <!--begin:Menu item-->
-                        <div class="menu-item">
-                            <!--begin:Menu link-->
-                            <a class="menu-link" href="dashboards/delivery.html">
-                                <span class="menu-bullet">
-                                    <span class="bullet bullet-dot"></span>
-                                </span>
-                                <span class="menu-title">Delivery</span>
-                            </a>
-                            <!--end:Menu link-->
-                        </div>
-                        <!--end:Menu item-->
-                        <!--begin:Menu item-->
-                        <div class="menu-item">
-                            <!--begin:Menu link-->
-                            <a class="menu-link" href="dashboards/crypto.html">
-                                <span class="menu-bullet">
-                                    <span class="bullet bullet-dot"></span>
-                                </span>
-                                <span class="menu-title">Crypto</span>
-                            </a>
-                            <!--end:Menu link-->
-                        </div>
-                        <!--end:Menu item-->
-                        <!--begin:Menu item-->
-                        <div class="menu-item">
-                            <!--begin:Menu link-->
-                            <a class="menu-link" href="dashboards/school.html">
-                                <span class="menu-bullet">
-                                    <span class="bullet bullet-dot"></span>
-                                </span>
-                                <span class="menu-title">School</span>
-                            </a>
-                            <!--end:Menu link-->
-                        </div>
-                        <!--end:Menu item-->
-                        <!--begin:Menu item-->
-                        <div class="menu-item">
-                            <!--begin:Menu link-->
-                            <a class="menu-link" href="dashboards/podcast.html">
-                                <span class="menu-bullet">
-                                    <span class="bullet bullet-dot"></span>
-                                </span>
-                                <span class="menu-title">Podcast</span>
-                            </a>
-                            <!--end:Menu link-->
-                        </div>
-                        <!--end:Menu item-->
-                        <!--begin:Menu item-->
-                        <div class="menu-item">
-                            <!--begin:Menu link-->
-                            <a class="menu-link" href="landing.html">
-                                <span class="menu-bullet">
-                                    <span class="bullet bullet-dot"></span>
-                                </span>
-                                <span class="menu-title">Landing</span>
-                            </a>
-                            <!--end:Menu link-->
-                        </div>
-                        <!--end:Menu item-->
-                    </div>
-                    <div class="menu-item">
-                        <div class="menu-content">
-                            <a class="btn btn-flex btn-color-primary d-flex flex-stack fs-base p-0 ms-2 mb-2 toggle collapsible collapsed" data-bs-toggle="collapse" href="#kt_app_sidebar_menu_dashboards_collapse" data-kt-toggle-text="Show Less">
-                                <span data-kt-toggle-text-target="true">Show 12 More</span>
-                                <i class="ki-outline ki-minus-square toggle-on fs-2 me-0"></i>
-                                <i class="ki-outline ki-plus-square toggle-off fs-2 me-0"></i>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-                <!--end:Menu sub-->
+             
             </div>
             <!--end:Menu item-->
             <!--begin:Menu item-->
@@ -482,22 +243,17 @@
                 </span>
                 <!--end:Menu link-->
                 <!--begin:Menu sub-->
-                <div class="menu-sub menu-sub-dropdown px-2 py-4 w-200px w-lg-225px mh-75 overflow-auto">
+                <!-- <div class="menu-sub menu-sub-dropdown px-2 py-4 w-200px w-lg-225px mh-75 overflow-auto"> -->
                     <!--begin:Menu item-->
-                    <div class="menu-item">
+                    <!-- <div class="menu-item"> -->
                         <!--begin:Menu content-->
-                        <div class="menu-content">
-                            <span class="menu-section fs-5 fw-bolder ps-1 py-1">Reload</span>
-                        </div>
+                        <!-- <div class="menu-content"> -->
+                            <!-- <span class="menu-section fs-5 fw-bolder ps-1 py-1">Reload</span> -->
+                        <!-- </div> -->
                         <!--end:Menu content-->
-                    </div>
+                    <!-- </div> -->
                     <!--end:Menu item-->
-                    <!--begin:Menu item-->
-                    <div class="menu-item">
-                       
-                    </div>
-                    <!--end:Menu item-->
-                </div>
+                <!-- </div> -->
                 <!--end:Menu sub-->
             </div>
             <!--end:Menu item-->
@@ -511,31 +267,7 @@
                     </i>
                 </span>
                 <!--end:Menu link-->
-                <!--begin:Menu sub-->
-                <div class="menu-sub menu-sub-dropdown px-2 py-4 w-200px w-lg-225px mh-75 overflow-auto">
-                    <!--begin:Menu item-->
-                    <div class="menu-item">
-                        <!--begin:Menu content-->
-                        <div class="menu-content">
-                            <span class="menu-section fs-5 fw-bolder ps-1 py-1">API References</span>
-                        </div>
-                        <!--end:Menu content-->
-                         <!--begin:Menu item-->
-                        <div class="menu-item">
-                          <!--begin:Menu link-->
-                          <a class="menu-link" href="/api_references" target="_blank">
-                             <span class="menu-bullet">
-                                 <span class="bullet bullet-dot"></span>
-                             </span>
-                             <span class="menu-title">API References</span>
-                          </a>
-                        <!--end:Menu link-->
-                    </div>
-                    <!--end:Menu item-->
-                    </div>
-                    <!--end:Menu item-->
-                </div>
-                <!--end:Menu sub-->
+
             </div>
             <!--end:Menu item-->
               <!--begin:Menu item-->
@@ -548,31 +280,7 @@
                    </i>
                 </span>
                 <!--end:Menu link-->
-                <!--begin:Menu sub-->
-                <div class="menu-sub menu-sub-dropdown px-2 py-4 w-200px w-lg-225px mh-75 overflow-auto">
-                    <!--begin:Menu item-->
-                    <div class="menu-item">
-                        <!--begin:Menu content-->
-                        <div class="menu-content">
-                            <span class="menu-section fs-5 fw-bolder ps-1 py-1">Coverage</span>
-                        </div>
-                        <!--end:Menu content-->
-                        <!--begin:Menu item-->
-                        <div class="menu-item">
-                          <!--begin:Menu link-->
-                          <a class="menu-link" href="/cov" target="_blank">
-                             <span class="menu-bullet">
-                                 <span class="bullet bullet-dot"></span>
-                             </span>
-                             <span class="menu-title">Coverage</span>
-                          </a>
-                        <!--end:Menu link-->
-                    </div>
-                    <!--end:Menu item-->
-                    </div>
-                    <!--end:Menu item-->
-                </div>
-                <!--end:Menu sub-->
+
             </div>
             <!--end:Menu item-->
                  <!--begin:Menu item-->
@@ -585,31 +293,6 @@
                     </i>
                 </span>
                 <!--end:Menu link-->
-                <!--begin:Menu sub-->
-                <div class="menu-sub menu-sub-dropdown px-2 py-4 w-200px w-lg-225px mh-75 overflow-auto">
-                    <!--begin:Menu item-->
-                    <div class="menu-item">
-                        <!--begin:Menu content-->
-                        <div class="menu-content">
-                            <span class="menu-section fs-5 fw-bolder ps-1 py-1">Account Settings</span>
-                        </div>
-                        <!--end:Menu content-->
-                    </div>
-                    <!--end:Menu item-->
-                    <!--begin:Menu item-->
-                    <div class="menu-item">
-                        <!--begin:Menu link-->
-                        <a class="menu-link" href="/settings" target="_blank">
-                            <span class="menu-bullet">
-                                <span class="bullet bullet-dot"></span>
-                            </span>
-                            <span class="menu-title">Account Settings</span>
-                        </a>
-                        <!--end:Menu link-->
-                    </div>
-                    <!--end:Menu item-->
-                </div>
-                <!--end:Menu sub-->
             </div>
             <!--end:Menu item-->
         </div>
