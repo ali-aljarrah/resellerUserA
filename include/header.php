56153eb9a38d772e094df716b8b4f96243c0f673
ali-aljarrah <ali.jarrah.109@gmail.com>
2024-06-12 15:46:13 +0800

    edit some issues

--- include/header.php
+++ include/header.php
@@ -1,131 +1,131 @@
 <div id="kt_app_header" class="app-header d-flex">
-					<!--begin::Header container-->
-					<div class="app-container container-fluid d-flex align-items-center justify-content-between" id="kt_app_header_container">
-						<!--begin::Logo-->
-						<div class="app-header-logo d-flex flex-center">
-							<!--begin::Logo image-->
-							<a href="index.html">
-								<img alt="Logo" src="assets/media/logos/demo-58.svg" class="mh-25px" />
-							</a>
-							<!--end::Logo image-->
-							<!--begin::Sidebar toggle-->
-							<button class="btn btn-icon btn-sm btn-active-color-primary d-flex d-lg-none" id="kt_app_sidebar_mobile_toggle">
-								<i class="ki-outline ki-abstract-14 fs-1"></i>
-							</button>
-							<!--end::Sidebar toggle-->
-						</div>
-						<!--end::Logo-->
-						<div class="d-flex flex-lg-grow-1 flex-stack" id="kt_app_header_wrapper">
-							<div class="app-header-wrapper d-flex align-items-center justify-content-around justify-content-lg-between flex-wrap gap-6 gap-lg-0 mb-6 mb-lg-0" data-kt-swapper="true" data-kt-swapper-mode="{default: 'prepend', lg: 'prepend'}" data-kt-swapper-parent="{default: '#kt_app_content_container', lg: '#kt_app_header_wrapper'}">
-								<!--begin::Page title-->
-								<div class="d-flex flex-column justify-content-center">
-									<!--begin::Title-->
-									<!-- <h1 class="text-gray-900 fw-bold fs-6 mb-2">Chartmix - Finance Team</h1> -->
-									<!--end::Title-->
-									<!--begin::Breadcrumb-->
-									<ul class="breadcrumb breadcrumb-separatorless fw-semibold fs-base">
-										<!--begin::Item-->
-										<li class="breadcrumb-item text-muted">
-											<a href="index.html" class="text-muted text-hover-primary">Home</a>
-										</li>
-										<!--end::Item-->
-										<!--begin::Item-->
-										<li class="breadcrumb-item text-muted">/</li>
-										<!--end::Item-->
-										<!--begin::Item-->
-										<li class="breadcrumb-item black">Dashboard</li>
-										<!--end::Item-->
-									</ul>
-									<!--end::Breadcrumb-->
-								</div>
-								<!--end::Page title-->
-								<div class="d-none d-md-block h-40px border-start border-gray-200 mx-10"></div>
-								<div class="d-flex gap-3 gap-lg-8 flex-wrap">
-									<div class="d-flex align-items-center gap-2">
-										<!-- <div class="rounded d-flex flex-center w-40px h-40px flex-shrink-0 bg-warning">
-											<i class="ki-outline ki-abstract-13 fs-2 text-inverse-warning"></i>
-										</div> -->
-										<!-- <div class="d-flex flex-column">
-											<span class="fw-bold fs-base text-gray-900">Target A</span>
-											<span class="fw-semibold fs-7 text-gray-500">Uplift: 64%</span>
-										</div> -->
-									</div>
-									<div class="d-flex align-items-center gap-2">
-										<!-- <div class="rounded d-flex flex-center w-40px h-40px flex-shrink-0 bg-danger">
-											<i class="ki-outline ki-abstract-24 fs-2 text-inverse-danger"></i>
-										</div> -->
-										<!-- <div class="d-flex flex-column">
-											<span class="fw-bold fs-base text-gray-900">Target A</span>
-											<span class="fw-semibold fs-7 text-gray-500">Uplift: 64%</span>
-										</div> -->
-									</div>
-									<div class="d-flex align-items-center gap-2">
-										<!-- <div class="rounded d-flex flex-center w-40px h-40px flex-shrink-0 bg-primary">
-											<i class="ki-outline ki-abstract-25 fs-2 text-inverse-primary"></i>
-										</div>
-										<div class="d-flex flex-column">
-											<span class="fw-bold fs-base text-gray-900">Target A</span>
-											<span class="fw-semibold fs-7 text-gray-500">Uplift: 64%</span>
-										</div> -->
-									</div>
-									<!-- <a href="#" class="btn btn-icon border border-200 bg-gray-100 btn-color-gray-600 btn-active-primary ms-2 ms-lg-6">
-										<i class="ki-outline ki-plus fs-3"></i>
-									</a> -->
-								</div>
-							</div>
-							<!--begin::Navbar-->
-							<div class="app-navbar flex-shrink-0 gap-3 gap-lg-8">
-								<!--begin::My apps links-->
-								<button type="button" class="btn btn-sm btn-icon me-n3 px-3" data-kt-menu-trigger="{default: 'click'}" data-kt-menu-placement="bottom-end">
-									<i class="ki-duotone ki-magnifier icon-header mt-3">
- 										<span class="path1"></span>
- 										<span class="path2"></span>
-									</i>
-								</button>
-								<button type="button" class="btn btn-sm btn-icon me-n3 px-3" data-kt-menu-trigger="{default: 'click'}" data-kt-menu-placement="bottom-end">
-									<i class="ki-duotone ki-notification-on icon-header mt-3">
-  										<span class="path1"></span>
-  										<span class="path2"></span>
- 										<span class="path3"></span>
- 										<span class="path4"></span>
- 										<span class="path5"></span>
- 									</i>
-								</button>
-								<button type="button" class="btn btn-sm btn-icon me-n3 px-3" data-kt-menu-trigger="{default: 'click'}" data-kt-menu-placement="bottom-end">
-									<i class="ki-duotone ki-user icon-header mt-3">
-										<span class="path1"></span>
- 										<span class="path2"></span>
-									</i>
-								</button>
-
-								<a href="#" class="btn btn-primary btn-grad">
-								<i class="ki-duotone ki-crown-2 btn-crown">
-									<span class="path1"></span>
-									<span class="path2"></span>
-									<span class="path3"></span>
-								</i>									
-								Try Premium
-								</a>
-         
-         
-								<!-- <div class="app-navbar-item"> -->
-									<!--begin::Menu wrapper-->
-																
-									<!-- <div class="btn btn-icon border border-200 bg-gray-100 btn-color-gray-600 btn-active-color-primary w-40px h-40px" data-kt-menu-trigger="{default: 'click', lg: 'hover'}" data-kt-menu-attach="parent" data-kt-menu-placement="bottom-end">
-										<i class="ki-outline ki-element-11 fs-4"></i>
-									</div> -->
-									 
-									<!--begin::My apps-->
-									<!-- <div class="menu menu-sub menu-sub-dropdown menu-column w-100 w-sm-350px" data-kt-menu="true">										
-									</div> -->
-									<!--end::My apps-->
-									<!--end::Menu wrapper-->
-								<!-- </div> -->
-								<!--end::My apps links-->
-							
-							</div>
-							<!--end::Navbar-->
-						</div>
-					</div>
-					<!--end::Header container-->
+	<!--begin::Header container-->
+	<div class="app-container container-fluid d-flex align-items-center justify-content-between" id="kt_app_header_container">
+		<!--begin::Logo-->
+		<div class="app-header-logo d-flex flex-center">
+			<!--begin::Logo image-->
+			<a href="index.html">
+				<img alt="Logo" src="assets/media/logos/demo-58.svg" class="mh-25px" />
+			</a>
+			<!--end::Logo image-->
+			<!--begin::Sidebar toggle-->
+			<button class="btn btn-icon btn-sm btn-active-color-primary d-flex d-lg-none" id="kt_app_sidebar_mobile_toggle">
+				<i class="ki-outline ki-abstract-14 fs-1"></i>
+			</button>
+			<!--end::Sidebar toggle-->
+		</div>
+		<!--end::Logo-->
+		<div class="d-flex flex-lg-grow-1 flex-stack" id="kt_app_header_wrapper">
+			<div class="app-header-wrapper d-flex align-items-center justify-content-around justify-content-lg-between flex-wrap gap-6 gap-lg-0 mb-6 mb-lg-0" data-kt-swapper="true" data-kt-swapper-mode="{default: 'prepend', lg: 'prepend'}" data-kt-swapper-parent="{default: '#kt_app_content_container', lg: '#kt_app_header_wrapper'}">
+				<!--begin::Page title-->
+				<div class="d-flex flex-column justify-content-center">
+					<!--begin::Title-->
+					<!-- <h1 class="text-gray-900 fw-bold fs-6 mb-2">Chartmix - Finance Team</h1> -->
+					<!--end::Title-->
+					<!--begin::Breadcrumb-->
+					<ul class="breadcrumb breadcrumb-separatorless fw-semibold fs-base">
+						<!--begin::Item-->
+						<li class="breadcrumb-item text-muted">
+							<a href="index.html" class="text-muted text-hover-primary">Home</a>
+						</li>
+						<!--end::Item-->
+						<!--begin::Item-->
+						<li class="breadcrumb-item text-muted">/</li>
+						<!--end::Item-->
+						<!--begin::Item-->
+						<li class="breadcrumb-item black">Dashboard</li>
+						<!--end::Item-->
+					</ul>
+					<!--end::Breadcrumb-->
 				</div>
+				<!--end::Page title-->
+				<div class="d-none d-md-block h-40px border-start border-gray-200 mx-10"></div>
+				<div class="d-flex gap-3 gap-lg-8 flex-wrap">
+					<div class="d-flex align-items-center gap-2">
+						<!-- <div class="rounded d-flex flex-center w-40px h-40px flex-shrink-0 bg-warning">
+							<i class="ki-outline ki-abstract-13 fs-2 text-inverse-warning"></i>
+						</div> -->
+						<!-- <div class="d-flex flex-column">
+							<span class="fw-bold fs-base text-gray-900">Target A</span>
+							<span class="fw-semibold fs-7 text-gray-500">Uplift: 64%</span>
+						</div> -->
+					</div>
+					<div class="d-flex align-items-center gap-2">
+						<!-- <div class="rounded d-flex flex-center w-40px h-40px flex-shrink-0 bg-danger">
+							<i class="ki-outline ki-abstract-24 fs-2 text-inverse-danger"></i>
+						</div> -->
+						<!-- <div class="d-flex flex-column">
+							<span class="fw-bold fs-base text-gray-900">Target A</span>
+							<span class="fw-semibold fs-7 text-gray-500">Uplift: 64%</span>
+						</div> -->
+					</div>
+					<div class="d-flex align-items-center gap-2">
+						<!-- <div class="rounded d-flex flex-center w-40px h-40px flex-shrink-0 bg-primary">
+							<i class="ki-outline ki-abstract-25 fs-2 text-inverse-primary"></i>
+						</div>
+						<div class="d-flex flex-column">
+							<span class="fw-bold fs-base text-gray-900">Target A</span>
+							<span class="fw-semibold fs-7 text-gray-500">Uplift: 64%</span>
+						</div> -->
+					</div>
+					<!-- <a href="#" class="btn btn-icon border border-200 bg-gray-100 btn-color-gray-600 btn-active-primary ms-2 ms-lg-6">
+						<i class="ki-outline ki-plus fs-3"></i>
+					</a> -->
+				</div>
+			</div>
+			<!--begin::Navbar-->
+			<div class="app-navbar flex-shrink-0 gap-3 gap-lg-8 align-items-center">
+				<!--begin::My apps links-->
+				<button type="button" class="btn btn-sm btn-icon me-n3 px-3" data-kt-menu-trigger="{default: 'click'}" data-kt-menu-placement="bottom-end">
+					<i class="ki-duotone ki-magnifier icon-header mt-3">
+						<span class="path1"></span>
+						<span class="path2"></span>
+					</i>
+				</button>
+				<button type="button" class="btn btn-sm btn-icon me-n3 px-3" data-kt-menu-trigger="{default: 'click'}" data-kt-menu-placement="bottom-end">
+					<i class="ki-duotone ki-notification-on icon-header mt-3">
+						<span class="path1"></span>
+						<span class="path2"></span>
+						<span class="path3"></span>
+						<span class="path4"></span>
+						<span class="path5"></span>
+					</i>
+				</button>
+				<button type="button" class="btn btn-sm btn-icon me-n3 px-3" data-kt-menu-trigger="{default: 'click'}" data-kt-menu-placement="bottom-end">
+					<i class="ki-duotone ki-user icon-header mt-3">
+						<span class="path1"></span>
+						<span class="path2"></span>
+					</i>
+				</button>
+
+				<a href="#" class="btn btn-primary btn-grad">
+				<i class="ki-duotone ki-crown-2 btn-crown">
+					<span class="path1"></span>
+					<span class="path2"></span>
+					<span class="path3"></span>
+				</i>									
+				Try Premium
+				</a>
+
+
+				<!-- <div class="app-navbar-item"> -->
+					<!--begin::Menu wrapper-->
+												
+					<!-- <div class="btn btn-icon border border-200 bg-gray-100 btn-color-gray-600 btn-active-color-primary w-40px h-40px" data-kt-menu-trigger="{default: 'click', lg: 'hover'}" data-kt-menu-attach="parent" data-kt-menu-placement="bottom-end">
+						<i class="ki-outline ki-element-11 fs-4"></i>
+					</div> -->
+						
+					<!--begin::My apps-->
+					<!-- <div class="menu menu-sub menu-sub-dropdown menu-column w-100 w-sm-350px" data-kt-menu="true">										
+					</div> -->
+					<!--end::My apps-->
+					<!--end::Menu wrapper-->
+				<!-- </div> -->
+				<!--end::My apps links-->
+			
+			</div>
+			<!--end::Navbar-->
+		</div>
+	</div>
+	<!--end::Header container-->
+</div>
